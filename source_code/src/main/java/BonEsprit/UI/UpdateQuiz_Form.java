c22a0a22b2ce937f81b030075743a15aecb1c3a8
Dương Quang Vinh <duongquangvinh6@gmail.com>
2021-05-15 01:53:23 +0700

    UpdateQuizzV3

--- source_code/src/main/java/BonEsprit/UI/UpdateQuiz_Form.java
+++ source_code/src/main/java/BonEsprit/UI/UpdateQuiz_Form.java
@@ -6,6 +6,7 @@ import java.awt.Cursor;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,10 +15,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
 
 class QuestionPanel extends JPanel {
@@ -36,47 +36,62 @@ class QuestionPanel extends JPanel {
 	JTextField answerArea_4;
 	JTextField scoreArea_4;
 	
-	public QuestionPanel(int position) {
-		this.setBounds(5, position, 300, 320);
+	public QuestionPanel(int position, int num) {
+		this.setBounds(5, position, 400, 275);
 		this.setBackground(new Color(0xD3DEF2));
 		this.setLayout(null);
 		
-		questionLabel = new JLabel("Question: ");
-		questionLabel.setBounds(0, 5, 150, 30);
+		questionLabel = new JLabel("Question: " + num);
+		questionLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
+		questionLabel.setBounds(10, 5, 150, 30);
 		
 		questionArea = new JTextArea();
-		questionArea.setBounds(10, 40, 250, 40);
+		questionArea.setBounds(20, 40, 360, 40);
+		
 		
 		JLabel answerLabel = new JLabel("Answer: ");
-		answerLabel.setBounds(0, 150, 150, 30);
+		answerLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+		answerLabel.setBounds(20, 90, 150, 30);
 		
 		answerArea_1 = new JTextField();
-		answerArea_1.setBounds(5, 185, 150, 30);
+		answerArea_1.setBounds(20, 125, 200, 30);
+		answerArea_1.setBorder(null);
 		JLabel score_1 = new JLabel("Score: ");
-		score_1.setBounds(165, 185, 80, 30);
+		score_1.setBounds(230, 125, 80, 30);
 		scoreArea_1 = new JTextField();
-		scoreArea_1.setBounds(225, 185, 50, 30);
+		scoreArea_1.setBorder(null);
+		scoreArea_1.setBounds(290, 125, 90, 30);
+		scoreArea_1.setHorizontalAlignment(JTextField.CENTER);
 		
 		answerArea_2 = new JTextField();
-		answerArea_2.setBounds(5, 220, 150, 30);
+		answerArea_2.setBorder(null);
+		answerArea_2.setBounds(20, 160, 200, 30);
 		JLabel score_2 = new JLabel("Score: ");
-		score_2.setBounds(165, 220, 80, 30);
+		score_2.setBounds(230, 160, 80, 30);
 		scoreArea_2 = new JTextField();
-		scoreArea_2.setBounds(225, 220, 50, 30);
+		scoreArea_2.setBorder(null);
+		scoreArea_2.setBounds(290, 160, 90, 30);
+		scoreArea_2.setHorizontalAlignment(JTextField.CENTER);
 		
 		answerArea_3 = new JTextField();
-		answerArea_3.setBounds(5, 255, 150, 30);
+		answerArea_3.setBorder(null);
+		answerArea_3.setBounds(20, 195, 200, 30);
 		JLabel score_3 = new JLabel("Score: ");
-		score_3.setBounds(165, 255, 80, 30);
+		score_3.setBounds(230, 195, 80, 30);
 		scoreArea_3 = new JTextField();
-		scoreArea_3.setBounds(225, 255, 50, 30);
-
+		scoreArea_3.setBorder(null);
+		scoreArea_3.setBounds(290, 195, 90, 30);
+		scoreArea_3.setHorizontalAlignment(JTextField.CENTER);
+		
 		answerArea_4 = new JTextField();
-		answerArea_4.setBounds(5, 290, 150, 30);
+		answerArea_4.setBorder(null);
+		answerArea_4.setBounds(20, 230, 200, 30);
 		JLabel score_4 = new JLabel("Score: ");
-		score_4.setBounds(165, 290, 80, 30);
+		score_4.setBounds(230, 230, 80, 30);
 		scoreArea_4 = new JTextField();
-		scoreArea_4.setBounds(225, 290, 50, 30);
+		scoreArea_4.setBorder(null);
+		scoreArea_4.setBounds(290, 230, 90, 30);
+		scoreArea_4.setHorizontalAlignment(JTextField.CENTER);
 		
 		this.add(questionLabel);
 		this.add(questionArea);
@@ -110,6 +125,7 @@ public class UpdateQuiz_Form extends JFrame {
 	private JButton submitButton;
 	private JButton clearButton;
 	private JButton cancelButton;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -135,7 +151,7 @@ public class UpdateQuiz_Form extends JFrame {
 	public UpdateQuiz_Form() {
 		setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 750);
+		setBounds(100, 100, 450, 780);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(0xFFFFFF));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -144,7 +160,7 @@ public class UpdateQuiz_Form extends JFrame {
 		
 		JLabel titleLabel = new JLabel("Title:");
 		titleLabel.setBounds(29, 10, 69, 47);
-		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+		titleLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
 		contentPane.add(titleLabel);
 		
 		titleTxtField = new JTextField();
@@ -160,35 +176,51 @@ public class UpdateQuiz_Form extends JFrame {
         	public void actionPerformed(ActionEvent e) {
         	}
         });
-        submitButton.setBounds(83, 650, 100, 40);
+        submitButton.setBounds(83, 692, 100, 40);
         contentPane.add(submitButton);
         
         cancelButton = new JButton("Cancel");
         cancelButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-        cancelButton.setBounds(266, 650, 100, 40);
+        cancelButton.setBounds(266, 692, 100, 40);
         cancelButton.setBackground(new Color(0x8D3E3E));
         cancelButton.setForeground(new Color(0xFFFFFF));
         contentPane.add(cancelButton);
         
-        JPanel questionPanel = new JPanelCustom(425, 1000);
+        JPanel questionPanel = new JPanelCustom(425, 290);
         questionPanel.setLocation(5, 80);
         questionPanel.setLayout(null);
-        
+
         JScrollPane questionScroll = new JScrollPane(questionPanel);
-        questionScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        questionScroll.setBorder(null);
         questionScroll.setBounds(5, 80, 425, 500);
+        questionScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        questionScroll.setBorder(null);
+        questionScroll.setViewportView(questionPanel);
         contentPane.add(questionScroll);
         
-        questionPanel.add(new QuestionPanel(5));
+        questionPanel.add(new QuestionPanel(5, 1));
         
-        questionPanel.add(new QuestionPanel(325));
+        
         
         addQuestion = new JButton("Add Question");
         addQuestion.setFont(new Font("Tahoma", Font.BOLD, 15));
         addQuestion.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         addQuestion.setBackground(new Color(0x85DEB9));
-        addQuestion.setBounds(270, 600, 150, 30);
+        addQuestion.setBounds(148, 591, 150, 40);
+        
+        addQuestion.addActionListener(new ActionListener() {
+        	int number = 2;
+            int position = 290;
+            int height = 290 * number;
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				questionPanel.setSize(425, height);
+				
+				questionPanel.add(new QuestionPanel(position, number));
+				position += 300;
+				number ++;
+				
+			}
+		});
         contentPane.add(addQuestion);
 
 	}
